cd98a724f93aab3a40798b6cf97d1936dfc37e1a
You You <you@mail.ru>
2019-11-06 18:08:43 +0500

    сделал бабочку и песочные часы

--- forif6.py
+++ forif6.py
@@ -1,7 +1,7 @@
 n = int(input("Введите n "))
 for i in range (n):
     for j in range(n):
-        if (i <= j and  i >= n-j-1) or (i >= j and i <= n-j-1) :
+        if (i <= j and i < n-j) or (i >= j and i >= n-j-1):
             print("*",end="")
         else:
             print(".",end="")
