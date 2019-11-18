fd5bc2d8517206b1c5e816d24419ce59e46762d6
You You <you@mail.ru>
2019-11-18 18:51:36 +0500

    доделал задачу с факториалом и деф.

--- факториал2.py
+++ факториал2.py
@@ -5,7 +5,5 @@ def fact (a):
     return k
 n = int(input("Введите число n: "))
 m = int(input("Введите число m: "))
-q = fact(n)
-
-R = q
-print (f"{R}")
+q = fact(n) / (fact (m) * fact(n - m))
+print (f"{q}")
