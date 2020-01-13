cbda17b0eabf1a2e00459a24617466316648c954
You You <you@mail.ru>
2020-01-13 20:05:59 +0500

    есть сумма нечётн

--- сумма_нечёт_рек.py
+++ сумма_нечёт_рек.py
@@ -1,9 +1,9 @@
 def sum (a): 
-    if a % 2 == 0:
-        a -= 1
     if a == 1:
         return 1
     return sum (a-2) + a
 a = int(input("Введите число: "))
+if a % 2 == 0:
+    a -= 1
 c = sum(a)
 print (c)
--- сумма_нечётных.py
+++ сумма_нечётных.py
@@ -1,8 +1,9 @@
 a = int(input("Введите число: "))
+b = 1
 if a % 2 == 0:
     a-=1
-b = 1
+d = a
 while b != a:
-    d = a + b
-    b = (b + 2) % a
+    d = d + b
+    b = b + 2
 print(d)
