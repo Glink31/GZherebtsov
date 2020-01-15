2097a5f9cae3513d755dee9095f30a2977037614
You You <you@mail.ru>
2020-01-15 18:32:56 +0500

    сделал 2 задачу с массивом, узнал про break

--- массив2.py
+++ массив2.py
@@ -1,11 +1,19 @@
+def mass (a):
+    for i,x in enumerate(a):
+        print (f'{i}. {x}')
 from random import randint
 a = []
 b = int(input("Введите размер массива: "))
 for i in range (b):
     c = randint(-5,10)
     a.append(c)
-c = int(input('Введите число'))
+mass (a)
+found = False
+c = int(input('Введите число: '))
 for i,x in enumerate(a):
-    print (f'{i}. {x}')
     if c == x:
         print(i)
+        found = True
+        break
+if not found:
+    print('такого числа нет')
