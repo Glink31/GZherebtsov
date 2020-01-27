d4b8cb22538296f57069e077e1a4fea8b5518257
You You <you@mail.ru>
2020-01-27 20:27:15 +0500

    нужно сделать сортировку вставками

--- сортировка_вставками.py
+++ сортировка_вставками.py
@@ -3,11 +3,10 @@ b = int(input('Введите размер массива: '))
 for i in range (b):
     a.append(int(input('Введите число массива: ')))
 print()
-for i in range (len(a)):
-    for j in range (i,len(a)):
-        if a[i] > a[j]:
-            a[i] = a[i] + a[j]
-            a[j] = a[i] - a[j]
-            a[i] = a[i] - a[j]
+for i in range (1,len(a)):
+    while a[i] < a[i-1]:
+        a[i] = a[i] + a[i-1]
+        a[i-1] = a[i] - a[i-1]
+        a[i] = a[i] - a[i-1]
 for i,x in enumerate(a):
     print(f'{i}. {x}')
