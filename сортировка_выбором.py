a3c41b2ffdded6cfcecdf2d2a0cf19462ac447a4
You You <you@mail.ru>
2020-02-03 19:56:32 +0500

    исправил сортировку выбором!

--- сортировка_выбором.py
+++ сортировка_выбором.py
@@ -3,11 +3,12 @@ b = int(input('Введите размер массива: '))
 for i in range (b):
     a.append(int(input('Введите число массива: ')))
 print()
-for i in range (len(a)):
+for i in range (len(a)-1):
+    c = i
     for j in range (i,len(a)):
-        if a[i] > a[j]:
-            a[i] = a[i] + a[j]
-            a[j] = a[i] - a[j]
-            a[i] = a[i] - a[j]
+        if a[c] > a[j]:
+            c = j
+    if i != c:
+        a[i], a[c] = a[c], a[i]
 for i,x in enumerate(a):
     print(f'{i}. {x}')
