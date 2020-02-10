a5b2091c3f1eb72853d2ebe43257a58a0bba9f50
You You <you@mail.ru>
2020-02-10 20:30:41 +0500

    проблемы с уходящими за края

--- поиск_половинами.py
+++ поиск_половинами.py
@@ -14,16 +14,19 @@ for i,x in enumerate(a):
     print(f'{i}. {x}')
 print()
 e1 = 0
-e2 = len(a)
-f = int(input('Введите искомое число: ')) 
-drop = False
-while not drop and e1 < e2:
-    if f == a[e2//2]:
-        print(e2//2)
-        drop = True
-    elif f > a[e2//2]:
-        e1 += e2//2
-    elif f < a[e2//2]:
-        e2 -= e2//2
-if not drop:
-    print('Такого числа нет')
+e2 = len(a)-1
+h = len(a)//2
+f = int(input('Введите искомое число: '))
+found = False 
+while e1!=e2 and not found:
+    if a[h] == f:
+        print(f'число найдено под номером {h}')
+        found = True
+    elif a[h] > f:
+        e2 = h-1
+        h = h-(e2-e1)-1 
+    elif a[h] < f:
+        e1 = h+1
+        h = h+(e2-e1)+1
+if not found:
+    print('такого числа нет')
