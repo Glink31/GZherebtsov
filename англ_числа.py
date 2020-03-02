c4855cf7954d242723681fed818298ac56dc86e0
You You <you@mail.ru>
2020-03-02 20:32:17 +0500

    страдание почти завершено

--- англ_числа.py
+++ англ_числа.py
@@ -4,37 +4,37 @@ teens = ['ten','eleven','twelve','thirteen','nineteen','sixteen','seventeen','ei
 orders = ['','thousand','million','billion']
 a = int(input('Введите число: '))
 x = 0
-while a > 0 and x < len(orders):
+k = ''
+while a != 0 and x < len(orders):
     exc = False
-    emp = True
+    emp = False
     hun = False
     b = a % 1000 
-    for i in range (len(ones)):
-        if i == b//100 and i == 0:
-            n3 = ['']
-        elif i == b//100:
-            n3 = [ones[i]]
-            hun = True
-            emp = False
-        if (b%100)//10 == 1 and i == a%10:
-            n2 = [teens[i]]
-            exc = True
-            emp = False
-        elif i == b and i > 1:
-            n2 = [tens[i]]
-            emp = False
-        else:
-            n2 = ['']
-        if i == b%10 and not exc:
-            n1 = [ones[i]]
-            emp = False
-        elif exc:
-            n1 = ['']
+    if b//100 == 0:
+        n3 = ''
+    elif b//100 > 0:
+        n3 = ones[b//100]
+        hun = True
+    if (b//10)%10 == 1:
+        n2 = teens[b%10]
+        exc = True 
+    elif (b//10)%10 > 1:
+        n2 = tens[(b//10)%10]
+    else:
+        n2 = ''
+    if not exc:
+        n1 = ones[b%10]
+    else:
+        n1 = ''
+    if b%10 == 0 and (b//10)%10 == 0 and b//100 == 0:
+        emp = True
     if not emp:
         if hun:
-            print(n3[0],'hundred ',n2[0],n1[0],orders[x],end='')
+            f=n3+' '+'hundred '+n2+' '+n1+' '+orders[x]+' '
+            k = f + k
         else:
-            print(n3[0],n2[0],n1[0],orders[x],end='')
+            f=n3+' '+n2+' '+n1+' '+orders[x]+' '
+            k = f + k
     a = a // 1000
     x += 1
-print() 
+print(k)
