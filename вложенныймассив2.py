0d802849b7e5afdebadda3f5c89c8e2634d25e06
You You <you@mail.ru>
2020-03-18 19:00:46 +0500

    а

--- вложенныймассив2.py
+++ вложенныймассив2.py
@@ -3,15 +3,20 @@ n = int(input('Введите количество команд: '))
 m = int(input('Введите количество соревнований: '))
 a = []
 b = []
-p = 0
 for i in range (n):
     y = 0
     a.append ([])
-    b.append([])
-    print(f'{i}. ',end='')
+    b.append ([])
     for j in range (m):
         c = randint(0,10)
         a[i].append(c)
         y = y+c
     b[i].append(i)
     b[i].append(y)
+for i in range (n):
+    maxs = 0
+    for k in range (1,len(b)):
+        if b[i][k]>b[i][k-1]:
+            maxs 
+    print(f'{i+1} место- {b[maxs][0]} команда, {b[maxs][1]}')
+    del b[maxs]
